457dc26c153ee4a7eda971fabfd482361135af19
ronney221 <ronneydo1@gmail.com>
2025-06-12 22:55:58 -0700

    changed angle degree of cards floating

--- app/(onboarding)/register.tsx
+++ app/(onboarding)/register.tsx
@@ -160,7 +160,7 @@ export default function RegisterScreen() {
                 opacity: 1,
                 translateY: 0,
                 scale: 1,
-                rotate: '0deg',
+                rotate: `${-5 + (index * 3)}deg`,
               }}
               transition={{
                 type: 'spring',
